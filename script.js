e3a7377794624e97279a2a2947b72c5e371605f3
introu <bart.kulak@gmail.com>
2022-02-20 18:24:45 +0100

    add game logic

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function computerPlay() {
 
 let playerScore = 0;
 let computerScore = 0;
+
 function gameRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return
@@ -30,7 +31,8 @@ function gameRound(playerSelection, computerSelection) {
     return 'How about you try to input Rock, Paper or Scissors, you rat?';
 
 }
-function game() {
+
+/*function game() {
     for (let i = 0; i < 5; i++) {
         let playerSelection = window.prompt('Rock, Paper or Scissors?').toLowerCase();
         let computerSelection = computerPlay();
@@ -47,7 +49,48 @@ function game() {
     }
     playerScore = 0;
     computerScore = 0;
-    console.log(finalText)
-}
+    alert(finalText)
+}*/
 
-document.getElementById("btn").onclick = game;
+
+const container = document.querySelector('#gameResults')
+
+let roundText = document.createElement('div');
+roundText.classList.add('roundText');
+container.appendChild(roundText);
+
+let scoreText = document.createElement('div');
+scoreText.classList.add('scoreText');
+scoreText.textContent = `Current player score: ${playerScore}, Current computer score: ${computerScore}`
+container.appendChild(scoreText);
+
+
+const allButtons = document.querySelectorAll('button.buttonStyle')
+allButtons.forEach(button => button.addEventListener('click', event => {
+    let computerPlaySign = computerPlay();
+    gameRound(event.target.id, computerPlaySign)
+    roundText.textContent = `Player has used ${event.target.id} and Computer has used ${computerPlaySign}!`
+    scoreText.textContent = `Current player score: ${playerScore}, Current computer score: ${computerScore}`
+    gameEndCheck()
+}));
+
+
+const resetGame = () => {
+    playerScore = 0;
+    computerScore = 0;
+    scoreText.textContent = `Current player score: ${playerScore}, Current computer score: ${computerScore}`
+};
+
+
+const resetButton = document.getElementById('resetGameButton')
+resetButton.addEventListener('click', resetGame);
+
+let gameEndCheck = () => {
+    if (playerScore >= 5) {
+        alert('Player wins!')
+        resetGame()
+    } else if (computerScore >= 5) {
+        alert('Computer wins!')
+        resetGame()
+    }
+}
